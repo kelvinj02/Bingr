39e89c311deea766029b1c12dfcec0f1269a191b
Kelvin <kjcoding02@gmail.com>
2026-03-29 17:01:45 -0700

    added routes and created database

---
+++ recommender/models.py
@@ -0,0 +1,31 @@
+from recommender import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__= 'users'
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(20), unique=True, nullable=False)
+    email=db.Column(db.String(120), unique=True, nullable=False)
+    password=db.Column(db.String(60), nullable=False)
+    comments=db.relationship('Comment', backref='author', lazy=True)
+    wishlist=db.relationship('WishListItem', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"    
+    
+class Comment(db.Model):
+    __tablename__= 'comments'
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    content=db.Column(db.Text, nullable=False)
+    created_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class WishListItem(db.Model):
+    __tablename__= 'wishlist_items'
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title=db.Column(db.String(60), nullable=False)
+    added_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"WishListItem('{self.title}','{self.added_at}')"
--- recommender/routes.py
+++ recommender/routes.py
@@ -1,8 +1,27 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
 from recommender import app
+from recommender.forms import RegistrationForm, LoginForm
 
 @app.route("/")
 @app.route("/home")
 def home():
     return "<h1>Hello World</h1>"
     #return render_template("index.html")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash("Your account has been created successfully!", "success")
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Sign Up", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    # if form.validate_on_submit():
+    #     if form.email.data == "example@gmail.com" and form.password.data == 'password':
+    #         flash("You have been login!", "success")
+    #     else:
+    #         flash("Login Unsuccessful. Please check email and password", "danger")
+    return render_template("login.html", title="Login", form=form)
